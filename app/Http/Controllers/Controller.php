3e090d6d5fc2f949630327411fac2d6c288413b5
Reach <L21390371@chetumal.tecnm.mx>
2026-06-01 00:47:59 -0500

    aplicacion vue y lv crud final sin css pq no venia en el tuto

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,45 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Crud;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Faker\Generator;
-
-class CrudController extends Controller
+abstract class Controller
 {
-    // READ - Obtener todos los Cruds
-    public function index()
-    {
-        return response(Crud::all()->toArray(), Response::HTTP_OK);
-    }
-
-    // CREATE - Crear un nuevo Crud aleatorio
-    public function create(Generator $faker)
-    {
-        $crud = new Crud();
-        $crud->name = $faker->lexify('????????');
-        $crud->color = $faker->boolean ? 'red' : 'green';
-        $crud->save();
-
-        return response($crud->toArray(), Response::HTTP_CREATED);
-    }
-
-    // UPDATE - Cambiar el color de un Crud
-    public function update(Request $request, $id)
-    {
-        $crud = Crud::findOrFail($id);
-        $crud->color = $request->color;
-        $crud->save();
-
-        return response(null, Response::HTTP_OK);
-    }
-
-    // DELETE - Eliminar un Crud
-    public function destroy($id)
-    {
-        Crud::destroy($id);
-
-        return response(null, Response::HTTP_OK);
-    }
+    //
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CrudController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get("/cruds", [CrudController::class, "index"]);
+Route::get("/cruds/create", [CrudController::class, "create"]);
+Route::put("/cruds/{id}", [CrudController::class, "update"]);
+Route::delete("/cruds/{id}", [CrudController::class, "destroy"]);
